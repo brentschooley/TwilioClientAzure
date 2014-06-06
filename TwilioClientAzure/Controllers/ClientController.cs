abf94ce82f9d0bc33839ed8abbc7a7cbb8fd9298
brentschooley <schooleybrent@gmail.com>
2014-06-06 15:41:36 -0700

    Initial push to GitHub

---
+++ TwilioClientAzure/Controllers/ClientController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio;
+using Twilio.TwiML;
+using Twilio.TwiML.Mvc;
+using System.Text.RegularExpressions;
+
+namespace TwilioClientAzure.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Token(string clientName)
+        {
+            if(String.IsNullOrEmpty(clientName))
+            {
+                clientName = "default";
+            }
+
+            var capability = new TwilioCapability(Credentials.AccountSid, Credentials.AuthToken);
+            capability.AllowClientIncoming(clientName);
+            capability.AllowClientOutgoing(Credentials.AppSid);
+
+            return Content(capability.GenerateToken());
+        }
+
+        public ActionResult Call(string source, string target)
+        {
+            var response = new TwilioResponse();
+
+            if (Regex.IsMatch(target, @"^[\d\+\-\(\) ]+$"))
+            {
+                response.Dial(new Number(target), new { callerId = source });
+            }
+            else
+            {
+                response.Dial(new Client(target), new { callerId = source });
+            }
+
+            return new TwiMLResult(response);
+        }
+    }
+}
---
+++ TwilioClientAzure/Credentials.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace TwilioClientAzure
+{
+    public static class Credentials
+    {
+        public static string AccountSid
+        {
+            get
+            {
+                return GetCredential("AccountSid");
+            }
+        }
+
+        public static string AuthToken
+        {
+            get
+            {
+                return GetCredential("AuthToken");
+            }
+        }
+
+        public static string AppSid
+        {
+            get
+            {
+                return GetCredential("AppSid");
+            }
+        }
+
+        public static string GetCredential(string credentialName)
+        {
+            var credential = ConfigurationManager.AppSettings[credentialName];
+            if(String.IsNullOrEmpty(credential))
+            {
+                credential = System.Environment.GetEnvironmentVariable(credentialName, EnvironmentVariableTarget.Machine);
+            }
+            return credential;
+        }
+    }
+}
